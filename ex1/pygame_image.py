465b406d2c24a981ace3d2359fb7a0d7a36a0399
asahi <c0a241491b@edu.teu.ac.jp>
2025-11-18 16:31:40 +0900

    こうかとんがぞうの追加（練習3）

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png") #操作したところ
+    kk_img = pg.transform.flip(kk_img,True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -17,7 +19,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(10)
 
 
