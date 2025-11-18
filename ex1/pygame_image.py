6b5befa92d387cb94ad389493b1f3b4ce53b4f01
asahi <c0a241491b@edu.teu.ac.jp>
2025-11-18 18:11:08 +0900

    演習課題2

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -16,6 +16,18 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200
     tmr = 0
+    def moveipf():
+        move_x = 0
+        move_y = 0 
+        if key_lst[pg.K_UP]:
+            move_y -= 1
+        elif key_lst[pg.K_DOWN]:
+            move_y += 1
+        elif key_lst[pg.K_LEFT]:
+            move_x -= 1
+        elif key_lst[pg.K_RIGHT]:
+            move_x += 1
+        return (move_x,move_y)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -26,15 +38,10 @@ def main():
         screen.blit(bg_img,[-x+3200,0])
 
         key_lst = pg.key.get_pressed()
-        kk_rct.move_ip((-1,0))
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+2,0))
+        kk_rct.move_ip(moveipf())
+
+        
+        
             
 
         screen.blit(kk_img,kk_rct)
